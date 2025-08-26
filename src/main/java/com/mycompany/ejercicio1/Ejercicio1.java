c8dafba19925fecd473005386e073acbac590d55
dilan <dilan@LAPTOP-A6QGST86>
2025-08-26 16:24:02 -0500

    Ejercicio 1

---
+++ src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dilan
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    //Calcule precio final de un producto aplicando un descuento ramdon el usuario ingresa el precio original y el descuento
+    System.out.print("Ingrese el precio original del producto ");
+    int producto = sc.nextInt();
+    System.out.print("Ingrese el descuento del producto");
+    int descuento = sc.nextInt();
+    
+    
+    }
+}
