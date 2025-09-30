04e0becaf1a26186660e9837f1a916139accace5
Dilan <dilanrealclansth9@gmail.com>
2025-09-30 16:18:17 -0500

    Progama de clase

---
+++ src/main/java/com/mycompany/trabajospod/sobrecarga.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.polimorfismo;
+
+/**
+ *
+ * @author dilan
+ */
+public class sobrecarga {
+    
+    int operacion(int n1, int n2){
+        return n1 + n2;
+    }
+
+    double operacion(double n1, double n2){
+        return n1 * n2;
+    }
+
+    int operacion(int n1, int n2, int n3){
+        return n1 + n2 + n3;
+    }
+    
+    public static void main(String[] args){
+        sobrecarga obj = new sobrecarga();  
+        System.out.println("Resultado: " + obj.operacion(2, 3));
+    }
+}
